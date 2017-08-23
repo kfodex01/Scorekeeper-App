34617474a756a41da413afc6a3c1c72ebf832882
kfodex01 <kafode@gmail.com>
2017-08-23 10:57:31 -0500

    Initial state

---
+++ Scorekeeper/app/src/main/java/com/austincreations/scorekeeper/StartingCash.java
@@ -0,0 +1,13 @@
+package com.austincreations.scorekeeper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartingCash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starting_cash);
+    }
+}
