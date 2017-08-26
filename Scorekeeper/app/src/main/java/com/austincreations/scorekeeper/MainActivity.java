590887f5339fe7c1150b38312a5d52bdb7eb733a
kfodex01 <kafode@gmail.com>
2017-08-26 10:22:05 -0500

    Removing Double Bar

--- Scorekeeper/app/src/main/java/com/austincreations/scorekeeper/MainActivity.java
+++ Scorekeeper/app/src/main/java/com/austincreations/scorekeeper/MainActivity.java
@@ -7,7 +7,9 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,10 +23,29 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // fills in the cash total
         tvCash = (TextView) findViewById(R.id.cash);
         Intent i = getIntent();
         cash = i.getLongExtra("cash", 0);
         tvCash.setText("$" + cash);
+
+        // sets event handlers for buttons
+        Button bnIncome = (Button) findViewById(R.id.income);
+        Button bnPayout = (Button) findViewById(R.id.payout);
+        bnIncome.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent i = new Intent(getApplicationContext(), TransactionActivity.class);
+                i.putExtra("willAdd", true);
+                startActivity(i);
+            }
+        });
+        bnPayout.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent i = new Intent(getApplicationContext(), TransactionActivity.class);
+                i.putExtra("willAdd", false);
+                startActivity(i);
+            }
+        });
 
     }
 
---
+++ Scorekeeper/app/src/main/java/com/austincreations/scorekeeper/TransactionActivity.java
@@ -0,0 +1,13 @@
+package com.austincreations.scorekeeper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TransactionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaction);
+    }
+}
